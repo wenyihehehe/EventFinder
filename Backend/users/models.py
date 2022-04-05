9c74c09dedcee85b6eb9ac1d4d637fc6ff8530f9
wenyihehehe <67746012+wenyihehehe@users.noreply.github.com>
2022-04-05 11:32:36 +0800

    minor fix

--- Backend/users/models.py
+++ Backend/users/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.base_user import BaseUserManager
 
-# Create your models here.
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, password, **extra_fields):
         if not email:
