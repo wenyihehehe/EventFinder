83bbe1c8f778d438a3d0cbbd529eacde24b625c8
wenyihehehe <67746012+wenyihehehe@users.noreply.github.com>
2022-04-11 21:33:51 +0800

    Add validation & linking

--- frontend/src/components/SignUpModal/index.js
+++ frontend/src/components/SignUpModal/index.js
@@ -1,13 +1,15 @@
 import './index.css';
 import React from 'react'
+import * as Auth from '../../services/auth';
+import swal from 'sweetalert';
 
 class SignUpModal extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            firstname: "",
-            lastname: "",
-            contactnumber: "",
+            firstName: "",
+            lastName: "",
+            contactNumber: "",
             email: "",
             password: "",
             privacy: false
@@ -27,31 +29,43 @@ class SignUpModal extends React.Component{
         });
     }
 
-    handleSubmit(event){
-        
-        this.validateForm();  
-        console.log(this.state);
+    async handleSubmit(event){
+        event.preventDefault()
+        let error = this.validateForm(); 
+        if(!error){
+            let signup = await Auth.signup({
+                email: this.state.email, 
+                password: this.state.password, 
+                firstName: this.state.firstName,
+                lastName: this.state.lastName,
+                contactNumber: this.state.contactNumber
+            });
+            if (signup.status === "OK"){
+                console.log("sign up successfully")
+            } else {
+                let errorMessage = Object.values(signup.detail)[0][0];
+                if(signup.detail.email) swal("Error!", errorMessage.charAt(0).toUpperCase() + errorMessage.slice(1), "error");
+            };
+        } 
     }
 
     validateForm(){
         // Fetch all the forms we want to apply custom Bootstrap validation styles to
         var forms = document.querySelectorAll('.needs-validation')
+        var error = false;
 
         // Loop over them and prevent submission
         Array.prototype.slice.call(forms)
-            .forEach(function (form) {
-            form.addEventListener('submit', function (event) {
-                if (!form.checkValidity()) {
-                    event.preventDefault()
-                    event.stopPropagation()
-                }
+        .forEach(function (form) {
+                if (!form.checkValidity()) error = true;
                 form.classList.add('was-validated')
-            }, false)
             });
-        
+            
         if(!this.state.privacy){
             document.querySelector(".checkmark").classList.add('invalid');
         }
+
+        return error;
     }
     
 
@@ -62,29 +76,35 @@ class SignUpModal extends React.Component{
                 <div className="title">Sign up with email</div>
                 <div className="description mt-1 mb-2">Already have an account? Sign In</div>
                 <form className="needs-validation" noValidate>
-                    <label htmlFor="firstname" className="form-label label">First Name</label>
+                    <label htmlFor="firstName" className="form-label label">First Name</label>
                     <div className="input-group mb-3">
-                        <input type="text" className="form-control" name="firstname" value={this.state.firstname} onChange={this.handleInputChange} required />
+                        <input type="text" className="form-control" name="firstName" value={this.state.firstName} onChange={this.handleInputChange} required maxLength={50} />
+                        <div className="invalid-feedback">This field is required.</div>
                     </div>
-                    <label htmlFor="lastname" className="form-label label">Last Name</label>
+                    <label htmlFor="lastName" className="form-label label">Last Name</label>
                     <div className="input-group mb-3">
-                        <input type="text" className="form-control" name="lastname" value={this.state.lastname} onChange={this.handleInputChange} required/>
+                        <input type="text" className="form-control" name="lastName" value={this.state.lastName} onChange={this.handleInputChange} required maxLength={50}/>
+                        <div className="invalid-feedback">This field is required.</div>
                     </div>
-                    <label htmlFor="contactnumber" className="form-label label">Contact Number</label>
+                    <label htmlFor="contactNumber" className="form-label label">Contact Number</label>
                     <div className="input-group mb-3">
-                        <input type="text" className="form-control" name="contactnumber" value={this.state.contactnumber} onChange={this.handleInputChange} required/>
+                        <input type="text" className="form-control" name="contactNumber" value={this.state.contactNumber} onChange={this.handleInputChange} required maxLength={11}/>
+                        <div className="invalid-feedback">This field is required.</div>
                     </div>
                     <label htmlFor="email" className="form-label label">Email Address</label>
                     <div className="input-group mb-3">
                         <input type="email" className="form-control" name="email" value={this.state.email} onChange={this.handleInputChange} required/>
+                        <div className="invalid-feedback">Please provide a valid email.</div>
                     </div>
                     <label htmlFor="password" className="form-label label">Password</label>
                     <div className="input-group mb-3">
-                        <input type="password" className="form-control" name="password" value={this.state.password} onChange={this.handleInputChange} required/>
+                        <input type="password" className="form-control" name="password" value={this.state.password} onChange={this.handleInputChange} required minLength={8}/>
+                        <div className="invalid-feedback">Password should be at least 8 characters.</div>
                     </div>
                 <label className="checkboxcontainer">
                     I agree to the Terms of Service and Privacy Policy
-                    <input type="checkbox" name="privacy" checked={this.state.privacy} onChange={this.handleInputChange} required/>
+                    <input type="checkbox" className="form-control" name="privacy" checked={this.state.privacy} onChange={this.handleInputChange} required/>
+                    <div className="invalid-feedback">You must agree to Terms of Service and Privacy Policy.</div>
                 <span className="checkmark"></span>
                 </label>
                 <button type="submit" className="btn primaryButton mt-1" style={{width:"100%"}} onClick={this.handleSubmit}>Submit</button>
--- frontend/src/routes/signup.js
+++ frontend/src/routes/signup.js
@@ -1,9 +1,10 @@
 import SignUpModal from "../components/SignUpModal";
+import { Link } from "react-router-dom";
 
 export default function SignUp() {
     return (
       <main className="container">
-        <img className="logo mt-2" src="logo.png" alt="logo"></img>
+        <Link to="/"><img className="logo mt-2" src="logo.png" alt="logo"></img></Link>
         <SignUpModal />
       </main>
     );
