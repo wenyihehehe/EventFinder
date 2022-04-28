ac38475e104b60f57b6ae51547705190eb7a1bb3
wenyihehehe <67746012+wenyihehehe@users.noreply.github.com>
2022-04-28 13:16:54 +0800

    Add big pagination

--- Backend/users/pagination.py
+++ Backend/users/pagination.py
@@ -35,3 +35,20 @@ class SmallPagination(PageNumberPagination):
             'max': max_number,
             'data': data
         })
+
+class BigPagination(PageNumberPagination):
+    page_size = 12
+    page_size_query_param = 'page_size'
+
+    def get_paginated_response(self, data):
+        max_number = math.ceil(self.page.paginator.count / self.page_size)
+        max_number = 1 if max_number<=0 else max_number
+        return Response({
+            'links': {
+                'next': self.get_next_link(),
+                'previous': self.get_previous_link()
+            },
+            'count': self.page.paginator.count,
+            'max': max_number,
+            'data': data
+        })
