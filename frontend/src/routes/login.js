a692f2fb241446f65fbe5a4c7ca1528416b62d5b
wenyihehehe <67746012+wenyihehehe@users.noreply.github.com>
2022-05-19 15:09:49 +0800

    Revamp navigate

--- frontend/src/routes/login.js
+++ frontend/src/routes/login.js
@@ -1,14 +1,20 @@
 import LoginModal from "../components/LoginModal";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
+import * as AuthProvider from './../config/authProvider'
 
 export default function LoginPage() {
-    let navigate = useNavigate();
-    return (
-      <main className="container-fluid row" style={{minHeight: "100vh"}}>
-        <div className="col-lg-6 pt-5 d-none d-lg-block" style={{backgroundColor : "#0659A8"}}>
-          <img className="img-fluid pt-5" src="illustration.png" alt="illustration"></img>
-        </div>
-        <LoginModal className="col-lg-6 justify-content-center" navigate={navigate}/>
-      </main>
-    );
-  }
+  let navigate = useNavigate();
+  let authContext = AuthProvider.useAuth();
+  let location = useLocation();
+
+  let from = location.state?.from?.pathname || "/";
+
+  return (
+    <main className="container-fluid row" style={{minHeight: "100vh"}}>
+      <div className="col-lg-6 pt-5 d-none d-lg-block" style={{backgroundColor : "#0659A8"}}>
+        <img className="img-fluid pt-5" src="illustration.png" alt="illustration"></img>
+      </div>
+      <LoginModal className="col-lg-6 justify-content-center" navigate={navigate} authContext={authContext} from={from}/>
+    </main>
+  );
+}
