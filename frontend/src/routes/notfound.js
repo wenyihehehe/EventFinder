d598614a50919c950809d49daab012bace3e536a
wenyihehehe <67746012+wenyihehehe@users.noreply.github.com>
2022-05-26 21:55:14 +0800

    Minor fix

--- frontend/src/routes/notfound.js
+++ frontend/src/routes/notfound.js
@@ -4,7 +4,7 @@ export default function NotFound() {
   return (
       <main className="row align-items-center justify-content-center mx-auto my-auto" style={{height:'90vh'}}>
         <div>
-          <p className="text-center SuperBigText tonedTextOrange">404</p>
+          <p className="text-center superBigText tonedTextOrange">404</p>
           <p className="text-center secondaryTitleText tonedTextOrange mb-3">Not Found</p>
           <p className="text-center detailMainText subTextColor">Sorry, we couldn't find that page.</p>
           <Link to='/'><button className="btn primaryButton w-100">Go to Homepage</button></Link>
