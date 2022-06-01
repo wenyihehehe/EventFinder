7d216d5b5e577502294c1d4de4d730f67e54f627
wenyihehehe <67746012+wenyihehehe@users.noreply.github.com>
2022-06-01 20:45:07 +0800

    Add link to edit profile

--- frontend/src/components/OrganizerProfile/index.js
+++ frontend/src/components/OrganizerProfile/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import style from './index.module.css';
 import * as User from '../../services/user';
+import { Link } from 'react-router-dom'
 
 class OrganizerProfile extends React.Component{
     constructor(props){
@@ -42,7 +43,7 @@ class OrganizerProfile extends React.Component{
         <div className={`${this.props.className } ${style.box} container row mt-5 pb-3`} style={{width: "85%"}}>
             <img className={`${style.profileImage} col-auto`} src={this.state.profileImage} alt="illustration"></img>
             <div className={`col align-self-center`}>
-                <p className="secondaryTitleText importantTextColor mb-1">{this.state.organizerName}</p>
+                <p className="secondaryTitleText importantTextColor mb-1">{this.state.organizerName}&nbsp;<Link to='/settings/organizeraccount'><i className="bi bi-pencil-fill" style={{fontSize: 'medium',verticalAlign: 'middle'}}></i></Link></p>
                 <p className="headingText mb-1">Description:</p>
                 <p className="detailMainText mb-2 twoLineTextClamp">{this.state.description ? this.state.description : "Not available"}</p>
                 <p className="detailMainText subTextColor">{this.state.events} Organize | {this.state.reviews} Review</p>
