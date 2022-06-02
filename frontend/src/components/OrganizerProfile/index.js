1f6a95f115ece4b85468b78ff9d471312ea3793e
wenyihehehe <67746012+wenyihehehe@users.noreply.github.com>
2022-06-02 23:32:35 +0800

    fix edit icon in view organizer page

--- frontend/src/components/OrganizerProfile/index.js
+++ frontend/src/components/OrganizerProfile/index.js
@@ -43,7 +43,7 @@ class OrganizerProfile extends React.Component{
         <div className={`${this.props.className } ${style.box} container row mt-5 pb-3`} style={{width: "85%"}}>
             <img className={`${style.profileImage} col-auto`} src={this.state.profileImage} alt="illustration"></img>
             <div className={`col align-self-center`}>
-                <p className="secondaryTitleText importantTextColor mb-1">{this.state.organizerName}&nbsp;<Link to='/settings/organizeraccount'><i className="bi bi-pencil-fill" style={{fontSize: 'medium',verticalAlign: 'middle'}}></i></Link></p>
+                <p className="secondaryTitleText importantTextColor mb-1">{this.state.organizerName}&nbsp;{this.props.organizerId ? "" : <Link to='/settings/organizeraccount'><i className="bi bi-pencil-fill" style={{fontSize: 'medium',verticalAlign: 'middle'}}></i></Link>}</p>
                 <p className="headingText mb-1">Description:</p>
                 <p className="detailMainText mb-2 twoLineTextClamp">{this.state.description ? this.state.description : "Not available"}</p>
                 <p className="detailMainText subTextColor">{this.state.events} Organize | {this.state.reviews} Review</p>
